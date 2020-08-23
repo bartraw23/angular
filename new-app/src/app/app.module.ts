3304d2de84c9f65b1aa4b3f086ea38e79b40b35c
Bartek <bartraw23@gmail.com>
2020-08-23 19:47:34 +0200

    Implement routing

--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HumansService } from './humans.service';
+import { AppRoutingModule } from './app-routing.module';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [HumansService],
   bootstrap: [AppComponent]
