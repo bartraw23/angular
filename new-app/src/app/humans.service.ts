73decd8a5a24f96ecf119ff9c1c287b2c0bc3c5b
Bartek <bartraw23@gmail.com>
2020-08-26 22:47:52 +0200

    Observables

--- new-app/src/app/humans.service.ts
+++ new-app/src/app/humans.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HumansService {
+
+  myObservable$ = new Observable( observer => {
+    observer.next();
+  });
+
   humans = [
     { name: 'Andrzej', age: 55 },
     { name: 'Sławek', age: 33 }
--- new-app/src/app/two/two.component.ts
+++ new-app/src/app/two/two.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HumansService } from 'src/app/humans.service'
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-two',
   templateUrl: './two.component.html',
   styleUrls: ['./two.component.css']
 })
-export class TwoComponent implements OnInit {
+export class TwoComponent implements OnInit, OnDestroy {
   humans = [];
 
   cash = 0;
+
+  anotherObservable$ = new Observable( observer => {
+    setInterval(()=>observer.next(new Date()), 1000)
+  });
 
   constructor(private humanService: HumansService,
     private route: ActivatedRoute,
@@ -20,8 +25,16 @@ export class TwoComponent implements OnInit {
   ngOnInit(): void {
     this.cash = +this.route.snapshot.paramMap.get('cash');
     this.humans = this.humanService.getAllHumans();
+
+    this.humanService.myObservable$.subscribe(() => {
+      this.cash += 10;
+    })
   }
 
+  ngOnDestroy(): void {
+    this.humanService.myObservable$.subscribe().unsubscribe();
+  }
+  
   back = function (evt) {
     this.location.back();
   }
